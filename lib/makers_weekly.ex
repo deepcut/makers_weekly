119f801ddfc5d1b9536667aa636e0fc06595428e
Feifan Wang <feifanw@gmail.com>
2017-04-13 23:36:46 -0500

    initial commit

---
+++ lib/makers_weekly.ex
@@ -0,0 +1,18 @@
+defmodule MakersWeekly do
+  @moduledoc """
+  Documentation for MakersWeekly.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MakersWeekly.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/makers_weekly_test.exs
@@ -0,0 +1,8 @@
+defmodule MakersWeeklyTest do
+  use ExUnit.Case
+  doctest MakersWeekly
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
