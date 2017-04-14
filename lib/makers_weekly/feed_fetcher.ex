9a267d72e4c7efce8ff12d4d8655e365d589b327
Feifan Wang <feifanw@gmail.com>
2017-04-14 09:31:18 -0500

    fetch all feeds async and parse the returned XML data

--- lib/makers_weekly/feed_fetcher.ex
+++ lib/makers_weekly/feed_fetcher.ex
@@ -1,5 +1,29 @@
 defmodule MakersWeekly.FeedFetcher do
-  use Tesla, only: ~w(get)a
-  adapter :ibrowse
+  @default_feed_data_file "data.yml"
 
+  use Tesla, only: ~w(get)a
+
+  adapter :ibrowse
+  plug Tesla.Middleware.FollowRedirects
+  plug Tesla.Middleware.Logger
+  # plug Tesla.Middleware.DebugLogger
+
+  def fetch_all do
+    parse_feed_data()
+    |> Enum.map(fn(sub) ->
+      Task.async(fn -> fetch(sub["feed_url"]) end)
+    end)
+    |> Enum.map(&Task.await(&1, :infinity))
+  end
+
+  def fetch(feed_url) do
+    get(feed_url).body
+    |> ElixirFeedParser.parse()
+  end
+
+  def parse_feed_data(file_name \\ @default_feed_data_file) do
+    file_name
+    |> Path.absname()
+    |> YamlElixir.read_from_file()
+  end
 end
